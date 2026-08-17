8d10d773bc4cb1df44d91a0aa05f09d4132b859a
Nicolas Kulpa-Hurault <nicolas.kulpa-hurault@hotmail.com>
2026-08-18 01:37:56 +0200

    feat(ex03): implement exercise 3 solution

--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,14 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+vowels = "aeiouyAEIOUY"
+
+words_vowels_count = []
+
+for word in words:
+    vowel_count = 0
+    for letter in word:
+        if letter in vowels:
+            vowel_count += 1
+    
+    words_vowels_count.append((word, vowel_count))
+
+print(words_vowels_count)
