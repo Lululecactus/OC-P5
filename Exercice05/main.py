a67b69318b945e24d04f0c38ad137d88ddc83ac1
Nicolas Kulpa-Hurault <nicolas.kulpa-hurault@hotmail.com>
2026-08-18 01:45:26 +0200

    feat(ex05): implement exercise 5 solution

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,24 @@
 def sum(a, b):
+    """Additionne deux nombres.
+
+    Args:
+        a (int or float): Le premier nombre.
+        b (int or float): Le second nombre.
+
+    Returns:
+        int or float: La somme de a et b.
+    """
     return a + b
- 
+
+
 def subtraction(a, b):
+    """Soustrait un nombre d'un autre.
+
+    Args:
+        a (int or float): Le nombre duquel on soustrait.
+        b (int or float): Le nombre à soustraire.
+
+    Returns:
+        int or float: Le résultat de a - b.
+    """
     return a - b
