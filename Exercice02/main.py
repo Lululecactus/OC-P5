082db1c77a738621d54ec9d9ccf487f6afd15c4a
Nicolas Kulpa-Hurault <nicolas.kulpa-hurault@hotmail.com>
2026-08-18 01:30:26 +0200

    feat(ex02): implement exercise 2 solution

--- Exercice02/main.py
+++ Exercice02/main.py
@@ -15,3 +15,15 @@ students = {
          'Histoire': 78
      }
 }
+
+name = input("Entrez le nom de l'étudiant : ")
+
+if name in students:
+    print(f"Notes de {name} : ")
+    for subject, grade in students[name].items():
+        print(f"{subject} : {grade}")
+
+    average = sum(students[name].values()) / len(students[name])
+    print(f"Moyenne de {name} : {average}")
+else:
+    print(f"L'étudiant {name} n'existe pas dans la liste.")
