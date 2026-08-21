18f7d3992b63985f386a29179165f2b6a7d1c902
Nicolas Kulpa-Hurault <nicolas.kulpa-hurault@hotmail.com>
2026-08-22 00:28:50 +0200

    feat(ex06): implement exercise 6 solution

--- Exercice06/main.py
+++ Exercice06/main.py
@@ -1,6 +1,16 @@
-# Fonction calculate_average
 def calculate_average(numbers):
+    """Calcule la moyenne d'une liste de nombres.
+
+    Args:
+        numbers (list): Une liste de nombres (int ou float).
+
+    Returns:
+        float: La moyenne des nombres. Renvoie 0 si la liste est vide.
+    """
+    if not numbers:
+        return 0
     return sum(numbers) / len(numbers)
+
 
 # Exemple d'utilisation de la fonction
 numbers = [10, 20, 30, 40, 50]
